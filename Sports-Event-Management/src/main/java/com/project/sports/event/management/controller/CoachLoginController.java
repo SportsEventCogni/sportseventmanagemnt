e82805198ac8902b15f3ba9f2e5c3f9d196483b7
Yasir FL45H <38528437+Sye7@users.noreply.github.com>
2020-03-30 01:01:23 +0530

    Final Update

--- Sports-Event-Management/src/main/java/com/project/sports/event/management/controller/CoachLoginController.java
+++ Sports-Event-Management/src/main/java/com/project/sports/event/management/controller/CoachLoginController.java
@@ -1,5 +1,10 @@
 package com.project.sports.event.management.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +14,36 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.project.sports.event.management.model.Coach;
+import com.project.sports.event.management.model.CoachRegis;
 import com.project.sports.event.management.model.Credentials;
+import com.project.sports.event.management.model.Event;
+import com.project.sports.event.management.model.NotificationCoach;
 import com.project.sports.event.management.model.Organizer;
+import com.project.sports.event.management.model.Sponsor;
+import com.project.sports.event.management.model.SponsorRegis;
 import com.project.sports.event.management.repository.CoachRepository;
+import com.project.sports.event.management.repository.CoachReqRepos;
+import com.project.sports.event.management.repository.EventRepository;
+import com.project.sports.event.management.repository.NotificationCoachRepos;
 
 @Controller
+@SessionAttributes( {"coachCreden", "eventList"} )
 public class CoachLoginController {
 	@Autowired
 	CoachRepository coachRepository;
 
+	@Autowired
+	EventRepository eventRepository;
+	
+	@Autowired
+	CoachReqRepos coachReqRepos;
+
+	@Autowired
+	NotificationCoachRepos notificationCoachRepos;
+	
 	
 	
 	
@@ -32,12 +56,14 @@ public class CoachLoginController {
 	
 
 	// Coach Login
-	@RequestMapping(value = "/coachSub", method = RequestMethod.GET)
-	public String validateCoachLogin(@ModelAttribute("credentials") Credentials credentials, ModelMap map) {
+	@RequestMapping(value = "/coachSub", method = RequestMethod.POST)
+	public String validateCoachLogin(@ModelAttribute("credentials") Credentials credentials, ModelMap map, HttpSession session) {
 		Coach coach = coachRepository.getCoach(credentials.getId(), credentials.getPassword());
 	
 		if (coach != null) {
-			return "home";
+			
+			session.setAttribute("coachCreden", credentials);
+			return "redirect:/coachHome";
 		} 
 		
 		map.addAttribute("failed", "Credentials does not matched");
@@ -54,7 +80,7 @@ public class CoachLoginController {
 	}
 
 	// coach Submission
-	@RequestMapping(value = "/coachRegisterUser", method = RequestMethod.GET)
+	@RequestMapping(value = "/coachRegisterUser", method = RequestMethod.POST)
 	public String registerUser(@Valid @ModelAttribute("coach") Coach coach, BindingResult bindingResult, ModelMap map) {
 		
 		if (bindingResult.hasErrors()) {
@@ -66,6 +92,114 @@ public class CoachLoginController {
 		map.addAttribute("credentials", new Credentials());
 		return "coachLogin";
 	}
+	
+	
+	
+	
+	@RequestMapping("/coachHome")
+	public String coachHome( ModelMap map, HttpSession session) {
+		
+		List<Event> li = eventRepository.findAll();
+
+		session.removeAttribute("eventList");
+		session.setAttribute("eventList", li);
+	
+		return "coachHome";
+	}
+	
+	
+	@RequestMapping("/requestSponsorshipCoach")
+	public String requestSponsorshipCoach(HttpServletRequest request, HttpSession session, ModelMap map) {
+
+		Credentials credentials = (Credentials) session.getAttribute("coachCreden");
+		String coachId = credentials.getId();
+		Coach coach = coachRepository.getOne(coachId);
+		
+		String coachName = coach.getFirstName()+" "+ coach.getLastName();
+		
+		String e = request.getParameter("EventRegistration");
+		String eventEntity[] = e.split(":");
+		
+		System.out.println(e);
+		
+		String eventId = eventEntity[0];
+		String eventName = eventEntity[1];
+		
+		CoachRegis coachReq = new CoachRegis(eventId, eventName, coachId, coachName);
+		
+		coachReqRepos.save(coachReq);
+		
+		List<Event> list =  (List<Event>) session.getAttribute("eventList");
+		
+		for(int i=0; i<list.size(); i++)
+		{
+			if(list.get(i).getEventId().equals(eventId) &&  list.get(i).getEventName().equals(eventName))
+			{
+				list.remove(i);
+				break;
+			}
+		}
+		
+		session.removeAttribute("eventList");
+		session.setAttribute("eventList", list);
+		
+
+		if(list.isEmpty())
+			session.invalidate();
+
+		
+		
+		return "redirect:/coachHome";
+	}
+	
+	
+	@RequestMapping("coachNotify")
+	public String coachNotification(ModelMap map, HttpSession session)
+	{
+		
+		Credentials credentials = (Credentials) session.getAttribute("coachCreden");
+		System.out.println(credentials+"yasir ");
+	
+		List<NotificationCoach> temp = notificationCoachRepos.findAll();
+		
+		List<NotificationCoach> notificationCoachList = new ArrayList<NotificationCoach>();
+		
+		for(int i=0; i<temp.size(); i++)
+		{
+			if(temp.get(i).getCoachId().equals(credentials.getId()))
+			{
+				notificationCoachList.add(temp.get(i));
+			}
+		}
+
+			
+		map.addAttribute("notificationCoachList", notificationCoachList);
+	
+		return "coachNotification";
+	}
+	
+
+	@RequestMapping("/remcoachNotify")
+	public String deleteNotification(HttpServletRequest request, HttpSession session, ModelMap map) {
+
+		Credentials credentials = (Credentials) session.getAttribute("coachCreden");
+		String coachId = credentials.getId();
+		
+		
+		String e = request.getParameter("EventRegistration");
+		String eventEntity[] = e.split(":");
+				
+		String eventId = eventEntity[0];
+		
+		notificationCoachRepos.deleteById(eventId);
+			
+		return "redirect:/coachNotify";
+	}
+	
+
+	
+	
+
 	
 	
 
--- Sports-Event-Management/src/main/java/com/project/sports/event/management/controller/OrganizerLoginController.java
+++ Sports-Event-Management/src/main/java/com/project/sports/event/management/controller/OrganizerLoginController.java
@@ -1,5 +1,8 @@
 package com.project.sports.event.management.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import javax.validation.Valid;
@@ -16,53 +19,69 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.project.sports.event.management.model.CoachRegis;
 import com.project.sports.event.management.model.Credentials;
+import com.project.sports.event.management.model.Event;
+import com.project.sports.event.management.model.NotificationCoach;
+import com.project.sports.event.management.model.NotificationSponsor;
 import com.project.sports.event.management.model.Organizer;
+import com.project.sports.event.management.model.Sponsor;
+import com.project.sports.event.management.model.SponsorRegis;
+import com.project.sports.event.management.repository.CoachReqRepos;
+import com.project.sports.event.management.repository.EventRepository;
+import com.project.sports.event.management.repository.NotificationCoachRepos;
+import com.project.sports.event.management.repository.NotificationSponsorRepos;
 import com.project.sports.event.management.repository.OrganizerRepository;
+import com.project.sports.event.management.repository.SponsorReqRepos;
 
 @Controller
+@SessionAttributes({ "sponsorList", "coachList" })
 public class OrganizerLoginController {
 
 	@Autowired
 	OrganizerRepository organizerRepository;
 
-	@RequestMapping("/home")
-	public String getHomePage() {
-		return "home";
-	}
-	
-	
+	@Autowired
+	EventRepository eventRepository;
+
+	@Autowired
+	SponsorReqRepos sponsorReqRepos;
+
+	@Autowired
+	CoachReqRepos coachReqRepos;
+
+	@Autowired
+	NotificationSponsorRepos notificationSponsorRepos;
+
+	@Autowired
+	NotificationCoachRepos notificationCoachRepos;
+
+
 
 	// Organizer Login page
 	@RequestMapping(value = "/organizerLogin", method = RequestMethod.GET)
 	public String getLoginPage(@ModelAttribute("credentials") Credentials credentials) {
-		 credentials = new Credentials();
+		credentials = new Credentials();
 		return "organizerLogin";
 	}
-	
-	
-	
-	
+
 	// Organizer Login Validation page
 
-    @RequestMapping(value = "/orgSub", method = RequestMethod.GET)
-    public String validateOrganizerRegistration(@ModelAttribute("credentials") Credentials credentials, HttpSession session, ModelMap map) {
-           Organizer org = organizerRepository.getOrganizer(credentials.getId(), credentials.getPassword());
-    
-           if (org != null) {
-                  session.setAttribute("id", credentials.getId());
-                  map.put("name", credentials.getId());
-                  return "organizerHome";
-           } 
-           
-           map.addAttribute("failed", "Credentials does not matched");
-                  return "organizerLogin";
-                        
-    }
+	@RequestMapping(value = "/orgSub", method = RequestMethod.GET)
+	public String validateOrganizerRegistration(@ModelAttribute("credentials") Credentials credentials,
+			HttpSession session, ModelMap map) {
+		Organizer org = organizerRepository.getOrganizer(credentials.getId(), credentials.getPassword());
 
-	
-	
+		if (org != null) {
+			session.setAttribute("id", credentials);
+			map.put("credentials", credentials);
+			return "organizerHome";
+		}
 
+		map.addAttribute("failed", "Credentials does not matched");
+		return "organizerLogin";
+
+	}
 
 	// Show organizer Registration Page
 	@RequestMapping("/organizerRegistration")
@@ -72,7 +91,7 @@ public class OrganizerLoginController {
 	}
 
 	// Organizer Registration Submission
-	
+
 	@RequestMapping(value = "/organizerRegisterUser", method = RequestMethod.GET)
 	public String registerUser(@Valid @ModelAttribute("organizer") Organizer organizer, BindingResult bindingResult,
 			ModelMap map) {
@@ -87,15 +106,145 @@ public class OrganizerLoginController {
 		map.addAttribute("credentials", new Credentials());
 		return "organizerLogin";
 	}
-	
-	
+
 	@RequestMapping(value = "logout", method = RequestMethod.GET)
-    public String logout(HttpSession session) {
-           session.removeAttribute("name");
-           return "home";
-    }
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/home";
+	}
+
+	@RequestMapping("/sponsorRequests")
+	public String sponsorRequests(@ModelAttribute("sponsorregis") SponsorRegis sponsorRegis, ModelMap m,
+			HttpSession session) {
+		List<SponsorRegis> sponsorList = sponsorReqRepos.findAll();
+
+		
+		m.addAttribute("sponsorList", sponsorList);
+
+		return "sponsorRequest";
+	}
+
+	@RequestMapping("/sponsorAcpt")
+	public String hello(ModelMap map, HttpServletRequest req, HttpSession session) {
+		String sponsorReq = (String) req.getParameter("EventRegistration");
+
+		String notify[] = sponsorReq.split(":");
+
+		String eventId = notify[0];
+		String sponsorId = notify[1];
+
+		String status = "Approved";
+		String request = "";
+
+		Event temp = eventRepository.getOne(eventId);
+		SponsorRegis sponsortemp = sponsorReqRepos.getSponsorRequest(eventId, sponsorId);
+
+		NotificationSponsor notification = new NotificationSponsor(temp.getEventId(), temp.getEventName(),
+				temp.getDate(), temp.getVenue(), sponsortemp.getSponsorId(), sponsortemp.getSponsorName(), status);
+
+		notificationSponsorRepos.save(notification);
+
+		sponsorReqRepos.deleteById(eventId);
+
+		map.put("req", request + " request is Accepted Successfully");
 
 	
+		return "redirect:/sponsorRequests";
+	}
 
-	
+	@RequestMapping("/sponsorReject")
+	public String hellodelete(ModelMap map, HttpServletRequest req, HttpSession session) {
+
+		String coachReq = (String) req.getParameter("EventRegistration");
+
+		String notify[] = coachReq.split(":");
+
+		String eventId = notify[0];
+		String sponsorId = notify[1];
+
+		String status = "Reject";
+		String request = "";
+
+		Event temp = eventRepository.getOne(eventId);
+		SponsorRegis sponsortemp = sponsorReqRepos.getSponsorRequest(eventId, sponsorId);
+
+		NotificationSponsor notification = new NotificationSponsor(temp.getEventId(), temp.getEventName(),
+				temp.getDate(), temp.getVenue(), sponsortemp.getSponsorId(), sponsortemp.getSponsorName(), status);
+
+		notificationSponsorRepos.save(notification);
+
+		sponsorReqRepos.deleteById(eventId);
+
+		map.put("req", request + " request is Accepted Successfully");
+		return "redirect:/sponsorRequests";
+
+	}
+
+	@RequestMapping("/coachRequests")
+	public String listRequest(@ModelAttribute("coachregis") CoachRegis coachRegis, ModelMap m, HttpSession session) {
+		List<CoachRegis> coachList = coachReqRepos.findAll();
+
+		m.addAttribute("coachList", coachList);
+
+		return "coachRequest";
+	}
+
+	@RequestMapping("/coachAcpt")
+	public String coachAcpt(ModelMap map, HttpServletRequest req, HttpSession session) {
+		String coachReq = (String) req.getParameter("EventRegistration");
+
+		String notify[] = coachReq.split(":");
+
+		String eventId = notify[0];
+		String coachId = notify[1];
+
+		String status = "Approved";
+		String request = "";
+
+		Event temp = eventRepository.getOne(eventId);
+		CoachRegis coachtemp = coachReqRepos.getCoachRequest(eventId, coachId);
+
+		NotificationCoach notification = new NotificationCoach(temp.getEventId(), temp.getEventName(), temp.getDate(),
+				temp.getVenue(), coachtemp.getCoachId(), coachtemp.getCoachName(), status);
+
+		notificationCoachRepos.save(notification);
+
+		coachReqRepos.deleteById(eventId);
+
+		map.put("req", request + " request is Accepted Successfully");
+		return "redirect:/coachRequests";
+	}
+
+	@RequestMapping("/coachReject")
+	public String coachDelete(ModelMap map, HttpServletRequest req, HttpSession session) {
+
+		String coachReq = (String) req.getParameter("EventRegistration");
+
+		String notify[] = coachReq.split(":");
+
+		String eventId = notify[0];
+		String coachId = notify[1];
+
+		String status = "Reject";
+		String request = "";
+
+		Event temp = eventRepository.getOne(eventId);
+		CoachRegis coachtemp = coachReqRepos.getCoachRequest(eventId, coachId);
+
+		NotificationCoach notification = new NotificationCoach(temp.getEventId(), temp.getEventName(), temp.getDate(),
+				temp.getVenue(), coachtemp.getCoachId(), coachtemp.getCoachName(), status);
+
+		notificationCoachRepos.save(notification);
+
+		coachReqRepos.deleteById(eventId);
+
+		map.put("req", request + " request is Accepted Successfully");
+		return "redirect:/coachRequests";
+	}
+
+	@RequestMapping("headerHome")
+	public String organizerHomeForHeader() {
+		return "organizerHome";
+	}
+
 }
